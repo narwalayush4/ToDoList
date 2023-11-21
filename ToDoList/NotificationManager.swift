9c3e63ceaf7c209db299cb05fe840ffd3fbe8f41
narwalayush4 <96654849+narwalayush4@users.noreply.github.com>
2023-11-22 05:19:43 +0530

    notification update

--- ToDoList/NotificationManager.swift
+++ ToDoList/NotificationManager.swift
@@ -21,31 +21,20 @@ class NotificationManager: ObservableObject {
     }
     
     func scheduleNotifications() {
-        // Remove existing notifications
-        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
         
-        // Schedule notifications every two hours
-        let timer = Timer.scheduledTimer(withTimeInterval: 2 * 60 * 60, repeats: true) { _ in
-            self.sendNotification(items: self.items)
-        }
-        // Ensure the timer keeps running even when the app is in the background
-        RunLoop.current.add(timer, forMode: .common)
+        let center = UNUserNotificationCenter.current()
+        center.removeAllPendingNotificationRequests()
+        
+        let content = UNMutableNotificationContent ()
+        content.title = "Reminder"
+        content.body = items.randomElement()?.text ?? ""
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 60, repeats: true)
+        
+        let request = UNNotificationRequest(identifier: UUID() .uuidString, content: content, trigger: trigger)
+        center.add(request)
+        
     }
     
-    func sendNotification(items: [Item]) {
-        let content = UNMutableNotificationContent()
-        content.title = "Reminder"
-        content.body = items.randomElement()?.text ?? "" // chooses a random element from the items present in the local storage
-        
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
-        
-        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-        
-        UNUserNotificationCenter.current().add(request) { error in
-            if let error = error {
-                print("Error scheduling notification: \(error.localizedDescription)")
-            }
-        }
-    }
     
 }
